958c2dbaf957164dd38c3f475e60a4be946fe055
unknown <sonwj0915@naver.com>
2021-03-23 15:27:37 +0900

    join 페이지 만들기

--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class RoomJoinPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: "",
+      error: "",
+    };
   }
   render() {
     return (
-      <div>
-        <p>This is the room join page</p>
-      </div>
+      <Grid container spacing={1} alignItems="center">
+        <Grid item xs={12}>
+          <Typography variant="h4" component="h4">
+            Join a Room
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            error={this.state.eroor}
+            label="Code"
+            placeholder="Enter a Room Code"
+            value={this.state.roomCode}
+            helperText={this.state.error}
+            variant="outlined"
+          />
+        </Grid>
+      </Grid>
     );
   }
 }
