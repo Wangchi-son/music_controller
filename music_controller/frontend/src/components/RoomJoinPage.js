29c7bb2a8ade2c46539766b1c2e92bf6a65f1477
unknown <sonwj0915@naver.com>
2021-03-25 21:26:50 +0900

    sessions and leaving rooms

--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -66,7 +66,7 @@ export default class RoomJoinPage extends Component {
         code: this.state.roomCode,
       }),
     };
-    fetch("api/join-room/", requestOptions)
+    fetch("api/join-room", requestOptions)
       .then((response) => {
         if (response.ok) {
           this.props.history.push(`/room/${this.state.roomCode}`);
