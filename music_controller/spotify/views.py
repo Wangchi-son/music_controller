be833496406462d6bcf77d46726e95fc08880fe0
unknown <sonwj0915@naver.com>
2021-03-30 15:03:16 +0900

    spotify 연동

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from pip._vendor.requests import Request, post
-from .util import update_or_create_user_tokens
+from .util import update_or_create_user_tokens, is_spotify_authenticated
 
 
 class AuthURL(APIView):
@@ -46,3 +46,10 @@ def spotify_callback(request, format=None):
         request.session.session_key, access_token, token_type, expires_in, refresh_token)
 
     return redirect('frontend:')
+
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_authenticated(
+            self.request.session.session_key)
+        return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
