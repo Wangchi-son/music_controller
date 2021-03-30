210246946257c7085340e55abbc435090eafcf3b
unknown <sonwj0915@naver.com>
2021-03-30 14:32:31 +0900

    request access and refresh tokens

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .credentials import REDIRECT_URI, CLIENT_ID, CLIENT_SECRET
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from pip._vendor.requests import Request, post
+from .util import update_or_create_user_tokens
 
 
 class AuthURL(APIView):
@@ -37,3 +38,11 @@ def spotify_callback(request, format=None):
     refresh_token = response.get('refresh_token')
     expires_in = response.get('expires_in')
     error = response.get('error')
+
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+
+    update_or_create_user_tokens(
+        request.session.session_key, access_token, token_type, expires_in, refresh_token)
+
+    return redirect('frontend:')
