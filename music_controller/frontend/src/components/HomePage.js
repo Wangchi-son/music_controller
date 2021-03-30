5f403cc90c594485df418239ad956079fb5a4813
unknown <sonwj0915@naver.com>
2021-03-30 23:21:53 +0900

    info 버튼 만들기

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -11,6 +11,7 @@ import {
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import Info from "./info";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -42,6 +43,9 @@ export default class HomePage extends Component {
             <ButtonGroup disableElevation variant="contained" color="primary">
               <Button color="primary" to="/join" component={Link}>
                 Join a Room
+              </Button>
+              <Button color="default" to="/info" component={Link}>
+                Info
               </Button>
               <Button color="secondary" to="/create" component={Link}>
                 Create a Room
@@ -75,6 +79,7 @@ export default class HomePage extends Component {
             }}
           />
           <Route path="/join" component={RoomJoinPage} />
+          <Route path="/info" component={Info} />
           <Route path="/create" component={CreateRoomPage} />
           <Route
             path="/room/:roomCode"
